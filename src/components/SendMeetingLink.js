3efd344efc44b591ac2a1fed8eb7dd5c98f3f41e
infinitymasters2023 <144238563+infinitymasters2023@users.noreply.github.com>
2023-12-19 17:28:56 +0530

    Meeting Url
    Meeting Url

--- src/components/SendMeetingLink.js
+++ src/components/SendMeetingLink.js
@@ -5,16 +5,16 @@ import { allowOnlyMobileNumber, allowOnlyEmailAddresses } from '../utils/helper'
 import { sendMeetingLinkAPI } from '../services/meeting_api'
 import AddMobileSendLink from './InputFields/HandleMobileSendLink'
 import HandleEmailSendLink from './InputFields/HandleEmailSendLink'
-
+import { useMeeting } from "@videosdk.live/react-sdk";
 const SendMeetingLink = ({ ticketInfo }) => {
+    const {meetingId} = useMeeting();
     const [requestBody, setRequestBody] = useState([]);
     const [otherMobile, setOtherMobile] = useState([]);
     const [otherEmail, setOtherEmail] = useState([]);
     const handleSendMeetingLink = async (e) => {
         e.preventDefault();
         const sendids = [...requestBody, ...otherMobile, ...otherEmail];
-        const storedLink = localStorage.getItem('meetingLink');
-        const iData = { sendTo: sendids, meetingLink: storedLink }
+        const iData = { sendTo: sendids, meetingLink: `https://meetings.infyshield.com/${meetingId}` }
         await sendMeetingLinkAPI(iData).then((response) => {
             if (response && response.isSuccess && response.statusCode === 200 && response.data) {
                 setRequestBody([])
